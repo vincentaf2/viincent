ef91f7342cbc722bb2ce116b4075b35234374803
vincentaf2 <113279388+vincentaf2@users.noreply.github.com>
2022-09-11 16:20:07 +0700

    Add files via upload

---
+++ no 6 c++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    for (i = 1; i < 6; i++) {
+        cout << "Belajar Koding di Kelas Algoritma" << endl;
+    }
+
+    return 0;
+}
